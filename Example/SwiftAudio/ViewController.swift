e2effcc6bace1794dd5b1b6f616edc752bd9ca9c
Jørgen Henrichsen <jh.henrichs@gmail.com>
2018-03-11 19:03:37 +0100

    Example project.

--- Example/SwiftAudio/ViewController.swift
+++ Example/SwiftAudio/ViewController.swift
@@ -7,18 +7,80 @@
 //
 
 import UIKit
+import SwiftAudio
+
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    
+    var isScrubbing: Bool = false
+    
+    lazy var player: AudioPlayer = {
+        let p = AudioPlayer(config: AudioPlayer.Config())
+        p.delegate = self
+        return p
+    }()
+    
+    @IBAction func playA(_ sender: Any) {
+        try? player.load(from: "https://p.scdn.co/mp3-preview/4839b070015ab7d6de9fec1756e1f3096d908fba")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func togglePlay(_ sender: Any) {
+        player.togglePlaying()
     }
+    
+    @IBAction func startScrubbing(_ sender: UISlider) {
+        isScrubbing = true
+    }
+    
+    @IBAction func scrubbing(_ sender: UISlider) {
+        player.seek(to: Double(slider.value))
+    }
+    
+    func update() {
+        slider.maximumValue = Float(player.duration)
+        slider.setValue(Float(player.currentTime), animated: true)
+    }
+    
+}
 
+extension ViewController: AudioPlayerDelegate {
+    
+    func audioPlayer(didChangeState state: AudioPlayerState) {
+        print("AudioPlayer state: ", state.rawValue)
+        if state == .playing {
+            playButton.setTitle("Pause", for: .normal)
+        }
+        else {
+            playButton.setTitle("Play", for: .normal)
+        }
+        
+        if state == .loading {
+            self.update()
+        }
+    }
+    
+    func audioPlayerItemDidComplete() {
+        
+    }
+    
+    func audioPlayer(secondsElapsed seconds: Int) {
+        print("Seconds elapsed")
+        if !isScrubbing {
+            slider.setValue(Float(seconds), animated: true)
+        }
+    }
+    
+    func audioPlayer(failedWithError error: Error) {
+        
+    }
+    
+    func audioPlayer(seekTo seconds: Int, didFinish: Bool) {
+        isScrubbing = false
+    }
+    
 }
 
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,5 +1,4 @@
 import XCTest
-import SwiftAudio
 
 class Tests: XCTestCase {
     
