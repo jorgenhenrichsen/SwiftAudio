80254146f778000cbf166637caa8d07213e3a721
Jørgen Henrichsen <jh.henrichs@gmail.com>
2018-03-18 18:12:48 +0100

    Audioplayer state tests.

--- Example/Tests/AudioPlayerTests.swift
+++ Example/Tests/AudioPlayerTests.swift
@@ -1,9 +1,163 @@
-//
-//  AudioPlayerTests.swift
-//  SwiftAudio_Tests
-//
-//  Created by Jørgen Henrichsen on 18/03/2018.
-//  Copyright © 2018 CocoaPods. All rights reserved.
-//
+import Quick
+import Nimble
 
-import Foundation
+@testable import SwiftAudio
+
+
+class AudioPlayerTests: QuickSpec {
+    
+    
+    override func spec() {
+        
+        let source = Bundle.main.path(forResource: "WAV-MP3", ofType: "wav")!
+        let shortSource = Bundle.main.path(forResource: "nasa_throttle_up", ofType: "mp3")!
+        
+        describe("An AudioPlayer") {
+            
+            var audioPlayer: AudioPlayer!
+            
+            beforeEach {
+                var config = AudioPlayer.Config()
+                config.automaticallyWaitsToMinimizeStalling = false
+                config.volume = 0.0
+                audioPlayer = AudioPlayer(config: config)
+            }
+            
+            describe("its state", {
+                
+                context("when doing nothing", {
+                    it("should be idle", closure: {
+                        expect(audioPlayer.state).to(equal(AudioPlayerState.idle))
+                    })
+                })
+                
+                context("when loading a source", {
+                    beforeEach {
+                        try? audioPlayer.load(fromFilePath: source, playWhenReady: false)
+                    }
+                    
+                    it("should eventually be ready", closure: {
+                        expect(audioPlayer.state).toEventually(equal(AudioPlayerState.ready))
+                    })
+                })
+                
+                context("when playing a source", {
+                    beforeEach {
+                        try? audioPlayer.load(fromFilePath: source, playWhenReady: true)
+                    }
+
+                    it("should eventually be playing", closure: {
+                        expect(audioPlayer.state).toEventually(equal(AudioPlayerState.playing))
+                    })
+
+                })
+
+                context("when pausing the source", {
+
+                    let holder = AudioPlayerDelegateHolder()
+
+                    beforeEach {
+                        audioPlayer.delegate = holder
+                        holder.stateUpdate = { (state) in
+                            if state == .playing {
+                                try? audioPlayer.pause()
+                            }
+                        }
+                        try? audioPlayer.load(fromFilePath: source, playWhenReady: true)
+                    }
+
+                    it("should eventually be paused", closure: {
+                        expect(audioPlayer.state).toEventually(equal(AudioPlayerState.paused))
+                    })
+
+                })
+
+                context("when stopping the source", {
+
+                    var holder: AudioPlayerDelegateHolder!
+                    var receivedIdleUpdate: Bool = false
+
+                    beforeEach {
+                        holder = AudioPlayerDelegateHolder()
+                        audioPlayer.delegate = holder
+                        holder.stateUpdate = { (state) in
+                            if state == .playing {
+                                audioPlayer.stop()
+                            }
+                            if state == .idle {
+                                receivedIdleUpdate = true
+                            }
+                        }
+                        try? audioPlayer.load(fromFilePath: source, playWhenReady: true)
+                    }
+
+                    it("should eventually be 'idle'", closure: {
+                        expect(receivedIdleUpdate).toEventually(beTrue())
+                    })
+
+                })
+                
+                context("when playing to the end of the track", {
+
+                    var holder: AudioPlayerDelegateHolder!
+                    var receivedIdleUpdate: Bool = false
+
+                    beforeEach {
+                        holder = AudioPlayerDelegateHolder()
+                        audioPlayer.delegate = holder
+                        holder.stateUpdate = { (state) in
+                            if state == .idle {
+                                receivedIdleUpdate = true
+                            }
+                        }
+                        try? audioPlayer.load(fromFilePath: shortSource, playWhenReady: true)
+                    }
+
+                    it("should eventually be idle", closure: {
+                        expect(receivedIdleUpdate).toEventually(beTrue(), timeout: 5.0, pollInterval: 1.0, description: nil)
+                    })
+
+                })
+                
+                
+            })
+        }
+        
+    }
+    
+}
+
+class AudioPlayerDelegateHolder: AudioPlayerDelegate {
+    
+    var state: AudioPlayerState? {
+        didSet {
+            if let state = state {
+                self.stateUpdate?(state)
+            }
+        }
+    }
+    
+    var stateUpdate: ((_ state: AudioPlayerState) -> Void)?
+    var itemDidComplete: (() -> Void)?
+    
+    func audioPlayer(didChangeState state: AudioPlayerState) {
+        self.state = state
+    }
+    
+    func audioPlayerItemDidComplete() {
+        
+    }
+    
+    func audioPlayer(secondsElapsed seconds: Double) {
+        
+    }
+    
+    func audioPlayer(failedWithError error: Error?) {
+        
+    }
+    
+    func audioPlayer(seekTo seconds: Int, didFinish: Bool) {
+        
+    }
+    
+}
