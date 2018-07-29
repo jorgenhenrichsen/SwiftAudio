a6e74efa377b0a8e4e15350d132b513204e2cb99
Jørgen Henrichsen <jh.henrichs@gmail.com>
2018-07-29 14:33:26 +0200

    Pass in the correct value in removeTarget(_:) when disabling remote commands.
    Use the actual pointer returned from addTarget(handler:).
    https://developer.apple.com/documentation/mediaplayer/mpremotecommand/1622910-addtarget

--- SwiftAudio/Classes/RemoteCommand.swift
+++ SwiftAudio/Classes/RemoteCommand.swift
@@ -14,22 +14,25 @@ public typealias RemoteCommandHandler = (MPRemoteCommandEvent) -> MPRemoteComman
 public protocol RemoteCommandProtocol {
     associatedtype Command: MPRemoteCommand
     
+    var id: String { get }
     var commandKeyPath: KeyPath<MPRemoteCommandCenter, Command> { get }
     var handlerKeyPath: KeyPath<RemoteCommandController, RemoteCommandHandler> { get }
 }
 
 public struct BaseRemoteCommand: RemoteCommandProtocol {
     
-    public static let play = BaseRemoteCommand(commandKeyPath: \MPRemoteCommandCenter.playCommand, handlerKeyPath: \RemoteCommandController.handlePlayCommand)
+    public static let play = BaseRemoteCommand(id: "Play", commandKeyPath: \MPRemoteCommandCenter.playCommand, handlerKeyPath: \RemoteCommandController.handlePlayCommand)
     
-    public static let pause = BaseRemoteCommand(commandKeyPath: \MPRemoteCommandCenter.pauseCommand, handlerKeyPath: \RemoteCommandController.handlePauseCommand)
+    public static let pause = BaseRemoteCommand(id: "Pause", commandKeyPath: \MPRemoteCommandCenter.pauseCommand, handlerKeyPath: \RemoteCommandController.handlePauseCommand)
     
-    public static let stop = BaseRemoteCommand(commandKeyPath: \MPRemoteCommandCenter.stopCommand, handlerKeyPath: \RemoteCommandController.handleStopCommand)
+    public static let stop = BaseRemoteCommand(id: "Stop", commandKeyPath: \MPRemoteCommandCenter.stopCommand, handlerKeyPath: \RemoteCommandController.handleStopCommand)
     
-    public static let togglePlayPause = BaseRemoteCommand(commandKeyPath: \MPRemoteCommandCenter.togglePlayPauseCommand, handlerKeyPath: \RemoteCommandController.handleTogglePlayPauseCommand)
+    public static let togglePlayPause = BaseRemoteCommand(id: "TogglePlayPause", commandKeyPath: \MPRemoteCommandCenter.togglePlayPauseCommand, handlerKeyPath: \RemoteCommandController.handleTogglePlayPauseCommand)
     
     
     public typealias Command = MPRemoteCommand
+    
+    public let id: String
     
     public var commandKeyPath: KeyPath<MPRemoteCommandCenter, MPRemoteCommand>
     
@@ -39,9 +42,11 @@ public struct BaseRemoteCommand: RemoteCommandProtocol {
 
 public struct ChangePlaybackPositionCommand: RemoteCommandProtocol {
     
-    public static let changePlaybackPosition = ChangePlaybackPositionCommand(commandKeyPath: \MPRemoteCommandCenter.changePlaybackPositionCommand, handlerKeyPath: \RemoteCommandController.handleChangePlaybackPositionCommand)
+    public static let changePlaybackPosition = ChangePlaybackPositionCommand(id: "ChangePlaybackPosition", commandKeyPath: \MPRemoteCommandCenter.changePlaybackPositionCommand, handlerKeyPath: \RemoteCommandController.handleChangePlaybackPositionCommand)
     
     public typealias Command = MPChangePlaybackPositionCommand
+    
+    public let id: String
     
     public var commandKeyPath: KeyPath<MPRemoteCommandCenter, MPChangePlaybackPositionCommand>
     
@@ -51,11 +56,13 @@ public struct ChangePlaybackPositionCommand: RemoteCommandProtocol {
 
 public struct SkipIntervalCommand: RemoteCommandProtocol {
     
-    public static let skipForward = SkipIntervalCommand(commandKeyPath: \MPRemoteCommandCenter.skipForwardCommand, handlerKeyPath: \RemoteCommandController.handleSkipForwardCommand)
+    public static let skipForward = SkipIntervalCommand(id: "SkipForward", commandKeyPath: \MPRemoteCommandCenter.skipForwardCommand, handlerKeyPath: \RemoteCommandController.handleSkipForwardCommand)
     
-    public static let skipBackward = SkipIntervalCommand(commandKeyPath: \MPRemoteCommandCenter.skipBackwardCommand, handlerKeyPath: \RemoteCommandController.handleSkipBackwardCommand)
+    public static let skipBackward = SkipIntervalCommand(id: "SkipBackward", commandKeyPath: \MPRemoteCommandCenter.skipBackwardCommand, handlerKeyPath: \RemoteCommandController.handleSkipBackwardCommand)
 
     public typealias Command = MPSkipIntervalCommand
+    
+    public let id: String
     
     public var commandKeyPath: KeyPath<MPRemoteCommandCenter, MPSkipIntervalCommand>
     
