7681d5d9835185739e6e4a1ac1e77b490e1de9e6
Jørgen Henrichsen <jh.henrichs@gmail.com>
2018-08-05 12:22:39 +0200

    Added AudioPlayerTests

---
+++ Example/Tests/AudioPlayerTests.swift
@@ -0,0 +1,140 @@
+import Quick
+import Nimble
+
+@testable import SwiftAudio
+
+class AudioPlayerTests: QuickSpec {
+    
+    override func spec() {
+        describe("An AudioPlayer") {
+            var audioPlayer: AudioPlayer!
+            
+            beforeEach {
+                audioPlayer = AudioPlayer()
+                audioPlayer.volume = 0
+            }
+            
+            describe("its state", {
+                
+                it("should be idle", closure: {
+                    expect(audioPlayer.playerState).to(equal(AudioPlayerState.idle))
+                })
+                
+                context("when audio item is loaded", {
+                    beforeEach {
+                        try? audioPlayer.loadItem(Source.getAudioItem(), playWhenReady: false)
+                    }
+                    
+                    it("it should eventually be ready", closure: {
+                        expect(audioPlayer.playerState).toEventually(equal(AudioPlayerState.ready))
+                    })
+                })
+                
+                context("when an item is loaded (playWhenReady=true)", {
+                    beforeEach {
+                        try? audioPlayer.loadItem(Source.getAudioItem(), playWhenReady: true)
+                    }
+                    
+                    it("it should eventually be playing", closure: {
+                        expect(audioPlayer.playerState).toEventually(equal(AudioPlayerState.playing))
+                    })
+                })
+                
+                context("when playing an item", {
+                    var holder: AudioPlayerDelegateHolder!
+                    beforeEach {
+                        holder = AudioPlayerDelegateHolder()
+                        audioPlayer.delegate = holder
+                        holder.stateUpdate = { state in
+                            if state == .ready {
+                                try? audioPlayer.play()
+                            }
+                        }
+                        try? audioPlayer.loadItem(Source.getAudioItem(), playWhenReady: false)
+                    }
+                    
+                    it("should eventually be playing", closure: {
+                        expect(audioPlayer.playerState).toEventually(equal(AudioPlayerState.playing))
+                    })
+                })
+                
+                context("when pausing an item", {
+                    var holder: AudioPlayerDelegateHolder!
+                    beforeEach {
+                        holder = AudioPlayerDelegateHolder()
+                        audioPlayer.delegate = holder
+                        holder.stateUpdate = { (state) in
+                            if state == .playing {
+                                try? audioPlayer.pause()
+                            }
+                        }
+                        try? audioPlayer.loadItem(Source.getAudioItem(), playWhenReady: true)
+                    }
+                    
+                    it("should eventually be paused", closure: {
+                        expect(audioPlayer.playerState).toEventually(equal(AudioPlayerState.paused))
+                    })
+                })
+                
+                context("when stopping an item", {
+                    var holder: AudioPlayerDelegateHolder!
+                    beforeEach {
+                        holder = AudioPlayerDelegateHolder()
+                        audioPlayer.delegate = holder
+                        holder.stateUpdate = { (state) in
+                            if state == .playing {
+                                audioPlayer.stop()
+                            }
+                        }
+                        try? audioPlayer.loadItem(Source.getAudioItem(), playWhenReady: true)
+                    }
+                    
+                    it("should eventually be idle", closure: {
+                        expect(audioPlayer.playerState).toEventually(equal(AudioPlayerState.idle))
+                    })
+                })
+                
+            })
+        }
+    }
+    
+}
+
+class AudioPlayerDelegateHolder: AudioPlayerDelegate {
+    
+    var stateUpdate: ((_ state: AudioPlayerState) -> Void)?
+    var state: AudioPlayerState? {
+        didSet {
+            if let state = state {
+                stateUpdate?(state)
+            }
+        }
+    }
+    
+    func audioPlayer(playerDidChangeState state: AudioPlayerState) {
+        self.state = state
+    }
+    
+    func audioPlayerItemDidComplete() {
+        
+    }
+    
+    func audioPlayer(secondsElapsed seconds: Double) {
+        
+    }
+    
+    func audioPlayer(failedWithError error: Error?) {
+        
+    }
+    
+    func audioPlayer(seekTo seconds: Int, didFinish: Bool) {
+        
+    }
+    
+    func audioPlayer(didUpdateDuration duration: Double) {
+        
+    }
+    
+}
+
+
