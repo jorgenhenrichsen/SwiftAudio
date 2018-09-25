0f2bfb3e79c24982a43ed971b1eafa72b24001e0
Jørgen Henrichsen <jh.henrichs@gmail.com>
2018-09-25 19:52:38 +0200

    Use long source when testing SimpleAudioPlayer

--- Example/Tests/SimpleAudioPlayerTests.swift
+++ Example/Tests/SimpleAudioPlayerTests.swift
@@ -22,7 +22,7 @@ class SimpleAudioPlayerTests: QuickSpec {
                 
                 context("when loading an item with playeWhenReady: false", {
                     beforeEach {
-                        try? player.load(item: ShortSource.getAudioItem(), playWhenReady: false)
+                        try? player.load(item: Source.getAudioItem(), playWhenReady: false)
                     }
                     it("should eventually be ready", closure: {
                         expect(player.playerState).toEventually(equal(AudioPlayerState.ready))
@@ -31,7 +31,7 @@ class SimpleAudioPlayerTests: QuickSpec {
                 
                 context("when loading an item with playWhenReady: true", {
                     beforeEach {
-                        try? player.load(item: ShortSource.getAudioItem(), playWhenReady: true)
+                        try? player.load(item: Source.getAudioItem(), playWhenReady: true)
                     }
                     it("should eventually be playing", closure: {
                         expect(player.playerState).toEventually(equal(AudioPlayerState.playing))
