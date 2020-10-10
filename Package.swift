4f37bb3b9c7a3d96d7892ac52be57f9355d1a0f9
Murilo Araujo <muernandes@icloud.com>
2020-10-10 15:21:35 -0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftAudio",
+    products: [
+        .library(
+            name: "SwiftAudio",
+            targets: ["SwiftAudio"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SwiftAudio",
+            dependencies: [],
+            path: "SwiftAudio/Classes")
+    ]
+)
